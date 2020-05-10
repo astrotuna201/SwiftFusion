4a8a12c64d9a177bffa38a849e886920972edd30
Marc Rasi <marcrasi@google.com>
2020-05-09 23:02:44 -0700

    alphabetize test registration

--- Tests/SwiftFusionTests/XCTestManifests.swift
+++ Tests/SwiftFusionTests/XCTestManifests.swift
@@ -5,15 +5,15 @@ public func allTests() -> [XCTestCaseEntry] {
   [
     testCase(AnyDifferentiableTests.allTests),
     testCase(BlockVectorTests.allTests),
+    testCase(CGLSTests.allTests),
     testCase(DictionaryDifferentiableTests.allTests),
     testCase(G2OReaderTests.allTests),
-    testCase(Rot2Tests.allTests),
-    testCase(Pose2Tests.allTests),
-    testCase(VectorTests.allTests),
-    testCase(NonlinearFactorGraphTests.allTests),
     testCase(GaussianFactorGraphTests.allTests),
-    testCase(CGLSTests.allTests),
+    testCase(NonlinearFactorGraphTests.allTests),
+    testCase(Pose2Tests.allTests),
+    testCase(Rot2Tests.allTests),
     testCase(ValuesTests.allTests),
+    testCase(VectorTests.allTests),
   ]
 }
 #endif
